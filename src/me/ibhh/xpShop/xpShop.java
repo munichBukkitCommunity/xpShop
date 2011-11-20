16f607f5f9f4af42ccac6fd84c7d94aa7b75f330
ibhh <weiterleitungen.email@googlemail.com>
2011-11-20 18:08:48 +0100

    Only one error on loading config!

--- src/me/ibhh/xpShop/xpShop.java
+++ src/me/ibhh/xpShop/xpShop.java
@@ -1,21 +1,13 @@
 package me.ibhh.xpShop;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.tehkode.permissions.PermissionManager;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
@@ -25,10 +17,7 @@ public class xpShop extends JavaPlugin {
 
 	
 	public iConomy iConomy = null;
-	private FileConfiguration messagesconfig = null;
-	private File messagesConfigurationFile = new File(getDataFolder(), "config.yml");
-	private FileConfiguration config;
-	private FileConfiguration messagesConfig;
+	protected FileConfiguration config;
 	
 	@Override
 	public void onDisable() {
@@ -40,35 +29,22 @@ public class xpShop extends JavaPlugin {
 	@Override
 	public void onEnable() {
 
-		loadConfiguration();
-		loadYamlConfiguration();
 		 //getServer().getPluginManager().registerEvent(Type.PLUGIN_ENABLE, new server(this), Priority.Monitor, this);
 	     //getServer().getPluginManager().registerEvent(Type.PLUGIN_DISABLE, new server(this), Priority.Monitor, this);
-
-	     
+		
+		
+		
 		try {
-			
-
-				
-				System.out.println("[xpShop] Config file not found!");
-				
-				loadYamlConfiguration();
-				
-				
-				System.out.println("[xpShop] could not be enabled: Config file was missing!");
-				return;
-			
+				config = getConfig();
+				System.out.println("[xpShop] Config file found!");		
 		}
 		catch (Exception e)
 		{
 			System.out.println("[xpShop] could not be enabled: Exception occured while seaching config file.");
+			createconfig();
 			e.printStackTrace();
-			return;
 		} 
-		
-		String xpcost = this.getConfig().getString("dbPath", "1");
-		String xpgetcost = this.getConfig().getString("dbUser", "1");
-		
+		System.out.println("[xpShop] Config file found!");	
 		System.out.println("[xpShop] successfully enabled!");
 		
 
@@ -98,27 +74,25 @@ public class xpShop extends JavaPlugin {
 			    	
 			    	if (cmd.getName().equalsIgnoreCase("xpShopxp")) {
 			    		if (args.length == 1) {
-			    			String name = Bukkit.getName();
 
 			    				
 			  
 			    		}
 				
 			    		else {
-			    			player.sendMessage(ChatColor.GRAY + "[xpShop]" + ChatColor.RED + "Dieser Befehl darf nur eine Zahl enthalten!");
+			    			player.sendMessage(ChatColor.GRAY + "[xpShop]" + ChatColor.RED + (getConfig().getString("command.error.toomanyarguments." + getConfig().getString("language"))));
 			    			succeed = false;
 			    		}
 			    	}
 			    } else {
-			    	player.sendMessage(ChatColor.GRAY + "[xpShop]" + ChatColor.RED + "Houston, Wir haben ein Problem :) Diesen Befehl darfst Du nicht verwenden!");
+			    	player.sendMessage(ChatColor.GRAY + "[xpShop]" + ChatColor.RED + (getConfig().getString("permission.error." + getConfig().getString("language"))));
 			    	succeed = false;
 			    	return false;
 
 			    }
 			} else {		//kein PermissionsEx
-			   Logger.getLogger("Minecraft").warning("PermissionsEx plugin nicht gefunden!");
+			   Logger.getLogger("Minecraft").warning((getConfig().getString("permission.notfound." + getConfig().getString("language"))));
 			}
-			
 			
 			
 		}
@@ -128,81 +102,27 @@ public class xpShop extends JavaPlugin {
 		
 	}
 	private void createconfig() {
-		
-		try {
-			this.getConfig().set("xpcost", "1");
-			this.getConfig().set("xpgetcost", "1");
-			this.saveConfig();
-			System.out.println("[xpShop] Config file created!");
-		}
-		catch (Exception e) {
-			
-			System.out.println("[xpShop] Config file could not be created: Exception occured while creating config file.");
-			e.printStackTrace();
-		}
-	}
-	
-	private void loadConfiguration() {
-		config = getConfig();
-		config.options().copyDefaults(true);
-		saveConfig();
-		}
-		public FileConfiguration getMessagesConfig() {
-		if (messagesconfig == null) {
-		reloadMessagesConfig();
-		}
-		return messagesconfig;
-		}
-		public void reloadMessagesConfig() {
-
-		    YamlConfiguration messagesconfig = new YamlConfiguration();
-
-		    try {
-
-		        messagesconfig.load(messagesConfigurationFile);
-
-		    } catch (IOException ex) {
-		    	System.out.println("[SimpleItemPermissions] messages config failed to load");
-		        return;
-		    } catch (InvalidConfigurationException ex) {
-		    	System.out.println("[SimpleItemPermissions] messages config failed to load");
-		        return;
-		    }
-
-		    //Do stuff here with config
-
-		}
-		
-		public void saveMessagesConfig() {
-		try {
-		messagesconfig.save(messagesConfigurationFile);
-		} catch (IOException ex) {
-		Logger.getLogger(JavaPlugin.class.getName()).log(Level.SEVERE, "Could not save config to " + messagesConfigurationFile, ex);
-		}
-		}
-		 
-		 
-		private void loadYamlConfiguration() {
-			final YamlConfiguration pluginconfig = new YamlConfiguration();
 
 			try {
+				getConfig().addDefault("language", "de");
+				getConfig().addDefault("command.error.toomanyarguments.de", "Dieser Befehl darf nur eine Zahl enthalten!");
+				getConfig().addDefault("command.error.toomanyarguments.en", "This command has only one number!");
+				getConfig().addDefault("permission.error.de", "Houston, Wir haben ein Problem :) Diesen Befehl darfst Du nicht verwenden!");
+				getConfig().addDefault("permission.error.en", "Houston, we have a problem! You can't use this command!");
+				getConfig().addDefault("permission.notfound.de", "PermissionsEx plugin nicht gefunden!");
+				getConfig().addDefault("permission.notfound.en", "PermissionsEx not found!");
+				//getConfig().options().copyDefaults(true);
+				this.saveConfig();
+				System.out.println("[xpShop] Config file created!");
+				}
+			catch (Exception e1)
+				{
+				System.out.println("[xpShop] Config file could not be created: Exception occured while creating config file.");
+				return;
+				}
+			
+	}
 
-			    pluginconfig.load("plugins/xpShop/config.yml");
-			    return;
-
-			} catch (IOException ex) {
-
-				System.out.println("[xpShop] Couldn't load config: " + ex.getMessage());
-				createconfig();
-			    return;
-
-			} catch (InvalidConfigurationException ex) {
-
-				System.out.println("[xpShop] Couldn't load config: " + ex.getMessage());
-			    return;
-
-			}
-		
 
   }
-}
+
