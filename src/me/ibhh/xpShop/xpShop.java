ff2a2fb781b85d52e9a0f6fbdc09abd8228b6e4e
ibhh <weiterleitungen.email@googlemail.com>
2011-11-21 17:22:18 +0100

    Config works!

--- src/me/ibhh/xpShop/xpShop.java
+++ src/me/ibhh/xpShop/xpShop.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.tehkode.permissions.PermissionManager;
@@ -17,7 +16,6 @@ public class xpShop extends JavaPlugin {
 
 	
 	public iConomy iConomy = null;
-	private FileConfiguration config;
 	
 	@Override
 	public void onDisable() {
@@ -26,17 +24,8 @@ public class xpShop extends JavaPlugin {
 		
 	}
 	
-    public void loadConfig(){
-        config.addDefault("language", "de");
-        config.addDefault("command.error.toomanyarguments.de", "Dieser Befehl darf nur eine Zahl enthalten!");
-        config.addDefault("command.error.toomanyarguments.en", "This command has only one number!");
-        config.addDefault("permission.error.de", "Houston, Wir haben ein Problem :) Diesen Befehl darfst Du nicht verwenden!");
-        config.addDefault("permission.error.en", "Houston, we have a problem! You can't use this command!");
-        config.addDefault("permission.notfound.de", "PermissionsEx plugin nicht gefunden!");
-        config.addDefault("permission.notfound.en", "PermissionsEx not found!");
-        config.options().copyDefaults(true);
-        saveConfig();
-    }
+
+
 
 
 	@Override
@@ -47,29 +36,12 @@ public class xpShop extends JavaPlugin {
 		
 		
 		
-		try {
-				config = getConfig();
-				loadConfig();
-				/*
-				File configfile = new File("plugins" + File.separator + "xpShop" + File.separator + "config.yml");
-				configfile.mkdir();
-				if(!config.contains("language"))
-				{config.set("language", "de");}
-				if(!config.contains("command.error.toomanyarguments.de"))
-				{config.set("command.error.toomanyarguments.de", "Dieser Befehl darf nur eine Zahl enthalten!");}
-				if(!config.contains("command.error.toomanyarguments.en"))
-				{config.set("command.error.toomanyarguments.en", "This command has only one number!");}
-				if(!config.contains("permission.error.de"))
-				{config.set("permission.error.de", "Houston, Wir haben ein Problem :) Diesen Befehl darfst Du nicht verwenden!");}
-				if(!config.contains("permission.error.en"))
-				{config.set("permission.error.en", "Houston, we have a problem! You can't use this command!");}
-				if(!config.contains("permission.notfound.de"))
-				{config.set("permission.notfound.de", "PermissionsEx plugin nicht gefunden!");}
-				if(!config.contains("permission.notfound.en"))
-				{config.set("permission.notfound.en", "PermissionsEx not found!");}
-				*/
-	
+		try
+		{
+
+				this.getConfig().options().copyDefaults(true);
 				saveConfig();
+				reloadConfig();
 				System.out.println("[xpShop] Config file found!");		
 				
 		}
@@ -130,11 +102,14 @@ public class xpShop extends JavaPlugin {
 			   Logger.getLogger("Minecraft").warning((getConfig().getString("permission.notfound." + getConfig().getString("language"))));
 			}
 			
+		succeed = true;
+		return succeed;	
+		} else {
 			
+		return false;
 		}
 		
-		succeed = true;
-		return succeed;
+
 		
 	}
 
